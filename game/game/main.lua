f856a814d70262bc4ee99f696d88c31e5f7e5a96
Milaan1337 <71323999+Milaan1337@users.noreply.github.com>
2022-09-17 12:40:58 +0200

    base structure
    alap struktúra

--- game/game/main.lua
+++ game/game/main.lua
@@ -1,18 +1,33 @@
-require "menu"
-require "settings"
+--[[
+Ez a main indítója. Ez fut le.
+]]
+
+require "menu" -- Ezzel importáljuk be más fájlok tartalmát, így tudunk fájlok közötti struktúrát használni
+require "settings" -- Ezzel importáljuk be más fájlok tartalmát, így tudunk fájlok közötti struktúrát használni
+---Ez a funkció fut le először(static void)
 function love.load()
+    --#region Main tábla felépítése
     main = {
+        --A jelenlegi képernyőt elmentjük a táblába
         currentScreen = {
           screen = nil,
           canvas = nil,
           name = nil,  
         },
+        -- Az előző képernyőt elmentjük a táblába
+        previousScreen = {
+            screen = nil,
+            canvas = nil,
+            name = nil,
+        },
+        -- A képernyőket táblába tároljuk a funkciójukkal is, ezáltal lelehet bontani az eseményeket screenekre.
         screens = {
             menu = {
-                canvas = love.graphics.newCanvas(),
-                name = "Főmenü",
-                draw = menu_draw,
-                click = menu_click,
+                canvas = love.graphics.newCanvas(), -- új canvas létrehozása(screen)
+                name = "Főmenü", -- title név(androidon nem kell)
+                draw = menu_draw, -- rajz funkció
+                click = menu_click, -- click funkció
+                --lehet bővíteni az események szerint
             },
             settings = {
                 canvas = love.graphics.newCanvas(),
@@ -23,19 +38,39 @@ function love.load()
         }
     }
     --
+    love.window.setFullscreen(true,"desktop") 
     main_setCanvas(main.screens.menu)
 end
-
+---Ez a funkció kezeli a képernyőváltásokat
+---@param screen screen
 function main_setCanvas(screen)
     if main.currentScreen.canvas ~= nil then
         love.graphics.clear()
+        main.previousScreen.screen = main.currentScreen.screen
+        main.previousScreen.canvas = main.currentScreen.canvas
+        main.previousScreen.name = main.currentScreen.name
+    else
+        main.previousScreen.screen = nil
+        main.previousScreen.canvas = nil
+        main.previousScreen.name = nil
     end
     main.currentScreen.screen = screen
     main.currentScreen.canvas = screen.canvas
     main.currentScreen.name = screen.name
     love.window.setTitle(main.currentScreen.name)
 end
-
+---Ez a funkció kezeli a visszanyíllal és/vagy escape-el történő képernyőváltást.
+function main_setPreviousCanvas()
+    if main.previousScreen.screen ~= nil then
+        main.currentScreen.screen = main.previousScreen.screen
+        main.currentScreen.canvas = main.previousScreen.canvas
+        main.currentScreen.name = main.previousScreen.name
+        love.window.setTitle(main.currentScreen.name) 
+    else
+        love.event.quit()
+    end
+end
+---Ez a funkció rajzolja ki minden képkockába a képernyőre a dolgokat.
 function love.draw()
     --HA VAN CANVAS
     if main.currentScreen.screen ~= nil then
@@ -45,7 +80,10 @@ function love.draw()
         love.graphics.draw(main.currentScreen.canvas)
     end
 end
-
+---Ez a funkció kezeli az egér és érintőképernyős eseményeket.
+---@param mx mouse_x
+---@param my mouse_y
+---@param button mouse_button 1|2|3
 function love.mousepressed(mx, my, button)
     if button == 1 then
         if main.currentScreen.screen ~= nil and main.currentScreen.screen.click ~= nil then
@@ -53,3 +91,14 @@ function love.mousepressed(mx, my, button)
         end
     end
 end
+---Ez a funkció kezeli a billentyűütéseket.Ide tartozik a mobilos visszanyíl, home gomb, stb...
+---@param key key key_names(wiki)
+---@param scancode scancode ez nem gomb-ra vonatkozik hanem a gomb poziciojara igy pl más billentyuzet kiosztason is ott lesz
+---@param isrepeat bool nyomások száma
+function love.keypressed( key, scancode, isrepeat )
+    if key ~= "escape" and key ~= "appback" then
+        print("ok")
+    else
+        main_setPreviousCanvas()
+    end
+ end
--- game/game/menu.lua
+++ game/game/menu.lua
@@ -1,3 +1,10 @@
+--[[
+    Menu screen
+    Minden képernyőnek új fájlt és saját táblát hozunk létre.
+]]
+--[[
+    Menü táblája - itt kezeljük a screen assets-eit, objectjeit ha van az átláthatóság miatt.
+]]
 menu_table = {
     assets = {
         images = {
@@ -22,16 +29,16 @@ menu_table = {
         }
     }
 }
-
+---Menü draw funkciója - a main.lua(draw) funkciója átirányítja az éppen aktív képernyő screenjére, ugyanez igaz a többi esemény-kezelő funkcióra is.
 function menu_draw()
     --#region Menü gombok
     local y_index = 0
     for i,v in pairs(menu_table.assets.images) do
         local w,h = v.img:getDimensions()
         local window_w,window_h = love.graphics.getDimensions()
-        love.graphics.draw(v.img,window_w/2 - w/2,y_index)
+        love.graphics.draw(v.img,window_w/2 - w/2,window_h/2 - (#menu_table.assets.images/2)*h + y_index)
         v.x = window_w/2 - w/2
-        v.y = y_index
+        v.y = window_h/2 - (#menu_table.assets.images/2)*h + y_index
         y_index = y_index + h + 15
     end
     --#endregion
@@ -49,7 +56,14 @@ function menu_click(x,y,button)
 end
 
 
-
+---Ez a funkció a megadott adatok alapján kiszámolja belekattintottunk-e a bizonyos boxba.
+---@param cx cursor_x egér x
+---@param cy cursor_y egér y
+---@param x box_x box x
+---@param y box_y box y
+---@param w box_w box szélessége
+---@param h box_h box magassága
+---@return bool isinbox benne van-e
 function isInBox(cx,cy,x,y,w,h)
     if cx >= x and cx<=x+w then
         if cy>=y and cy<= y+h then
@@ -58,7 +72,8 @@ function isInBox(cx,cy,x,y,w,h)
     end
     return false
 end
-
+--- Ez a funkció kezeli a menü gombok kattintás eseményeit.
+---@param button menu_button gomb azonosítója
 function menu_buttons_click(button)
     if button == "exit" then
         love.event.quit()
